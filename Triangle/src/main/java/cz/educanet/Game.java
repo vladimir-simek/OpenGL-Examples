4fc5bfe0ccca74dc2fc0006ecd8cde7e2586d486
vladimir-simek <minervladik@gmail.com>
2021-03-11 23:27:40 +0100

    bruh

--- Triangle/src/main/java/cz/educanet/Game.java
+++ Triangle/src/main/java/cz/educanet/Game.java
@@ -11,7 +11,7 @@ public class Game {
     private static final float vertices[] = {
             -0.5f, -0.5f, 0.0f,
             0.5f, -0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f, // Uncomment this for the square
+            0.5f, 0.5f, 0.0f,
             -0.5f, -0.5f, 0.0f,
             -0.5f, 0.5f, 0.0f,
             0.5f, 0.5f, 0.0f
